e84860a1e40e0d8b36847f03fdcfba4c07ac2a8b
Gabo <127222946+Herbei-Gabo@users.noreply.github.com>
2025-05-08 11:04:38 +0200

    Updates LogAnalyzerTask

--- MT_LogfileAnalyzer/src/main/java/LogAnalyzerTask.java
+++ MT_LogfileAnalyzer/src/main/java/LogAnalyzerTask.java
@@ -1,31 +1,34 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
 
-class LogAnalyzerTask implements Callable<Map<String, Integer>> {
-    private final Path filePath;
+public class LogAnalyzerTask implements Callable<Map<String, Integer>> {
+    private final Path logFile;
 
-    public LogAnalyzerTask(Path filePath) {
-        this.filePath = filePath;
+    public LogAnalyzerTask(Path logFile) {
+        this.logFile = logFile;
     }
 
     @Override
-    public Map<String, Integer> call() throws Exception {
-        Map<String, Integer> logLevelCount = new HashMap<>();
-        List<String> lines = Files.readAllLines(filePath);
-        for (String line : lines) {
-            for (String level : new String[]{"TRACE", "DEBUG", "INFO", "WARN", "ERROR"}) {
-                if (line.contains(level)) {
-                    logLevelCount.put(level, logLevelCount.getOrDefault(level, 0) + 1);
-                    break;
+    public Map<String, Integer> call() {
+        Map<String, Integer> logCounts = new HashMap<>();
+        try {
+            List<String> lines = Files.readAllLines(logFile);
+            for (String line : lines) {
+                for (String level : new String[]{"TRACE", "DEBUG", "INFO", "WARN", "ERROR"}) {
+                    if (line.contains(level)) {
+                        logCounts.put(level, logCounts.getOrDefault(level, 0) + 1);
+                        break;
+                    }
                 }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return logLevelCount;
+        return logCounts;
     }
 }
